bf7c0a09ce2b07042f032834ea2164b8926ee2a0
adrielmatos <84097666+adrielmatos@users.noreply.github.com>
2026-09-22 17:12:27 -0300

    ui: add CRM loading state

---
+++ app/loading.tsx
@@ -0,0 +1,1 @@
+export default function Loading(){return <main className="login"><div className="loginCard"><div className="brand big">A&K <em>Deskcomm</em></div><p>Carregando o Deskcomm…</p></div></main>}
